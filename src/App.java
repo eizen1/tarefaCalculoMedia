e788e65f33342a4ea86c2313cc4b1fd325d4edf6
Igor Sousa Bezerra <igorstraki@gmail.com>
2025-01-14 00:51:54 -0300

    Comitando tarefa

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     int nota1 = 5;
+     int nota2 = 9;
+     int nota3 = 10;
+     int nota4 = 6;
+
+     int media = (nota1+ nota2+nota3+nota4)/4;
+
+     System.out.println("Sua media foi "+ media);
+    }
+}
